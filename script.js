daf6105d68eaa2a847d5f939faed6d20729deeea
Nikolas <nikolaspeixoto986@gmail.com>
2025-08-08 21:53:55 -0300

    Começando com os eventos

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-let nomeUsuario = "William";
-const cpf = "139837128";
+
+const lista = document.querySelector("[data-list]");
+
+const novaTarefa = document.querySelector("[data-form-button]")
+
+const criarTarefa = (evento) =>{
+    evento.preventDefault();
+    const input = document.querySelector("[data-form-input]");
+    const valor = input.value;
+    console.log(valor);
+}
+
+
+// Evento
+novaTarefa.addEventListener('click', criarTarefa);
+//Espera um evento e oq vai acontecer
+
+// function somarDoisValores(num1, num2) {
+//     let soma = num1 + num2;
+//     console.log(some);
+// }
+
+// const diminuirDoisValores = (num1 , num2) => num1 - num2;
