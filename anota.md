c242f4d74da923706fbec80e4b2df4d7cf6f2792
Nikolas <nikolaspeixoto986@gmail.com>
2025-08-15 21:42:47 -0300

    Avançando com eventos

--- anota.md
+++ anota.md
@@ -7,3 +7,12 @@
 6. git push
 
 # Documentação do toDO
+`createElement()` -> Cria um novo elemento.
+`appendeChild()` -> Insere uma tag filha na tag pai.
+`innerHTML` -> Insere um elemento dentro de outro elemento na mesma hierarquia.
+`classList` -> Atributo que acessa a lista de classes da tag 
+`add()` -> Adiciona alguma coisa.
+
+Preciso pegar a constante conteudo, inserir ela dentro de uma tag li e inserir essa tag li na ul.
+
+Preciso incluir um botão no item toda vez que eu criar um novo item.
--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 
-const lista = document.querySelector("[data-list]");
-
-const novaTarefa = document.querySelector("[data-form-button]")
 
 const criarTarefa = (evento) =>{
     evento.preventDefault();
+    
+    const lista = document.querySelector("[data-list]");
     const input = document.querySelector("[data-form-input]");
     const valor = input.value;
-    console.log(valor);
+    
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+    const conteudo = `<p class="content">${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+    tarefa.appendChild(BotaoConclui());
+    lista.appendChild(tarefa);
+    
+    input.value = "";
+    
+    
+    // console.log(valor);
 }
 
 
 // Evento
+const novaTarefa = document.querySelector("[data-form-button]")
 novaTarefa.addEventListener('click', criarTarefa);
-//Espera um evento e oq vai acontecer
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button');
+
+    botaoConclui.classList.add('check-button');
+    botaoConclui.innerText = 'concluir'
+
+    botaoConclui.addEventListener('click', () => {
+        console.log('fui clicado');
+
+    });
+    return botaoConclui;
+};
+
+// `createElement()` -> Cria um novo elemento.
+// `appendeChild()` -> Insere um elemento filho no elemento pai.
+
 
 // function somarDoisValores(num1, num2) {
 //     let soma = num1 + num2;
